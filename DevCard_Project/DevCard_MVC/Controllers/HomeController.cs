197367565bd134d4e221e3f59bf8d8e1d2f81260
Ali_P Co <Ali_P Co@ALI-P-CO>
2021-04-15 16:31:26 +0430

    Index & Contact Page Added

--- DevCard_Project/DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_Project/DevCard_MVC/Controllers/HomeController.cs
@@ -11,22 +11,20 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+
+
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
